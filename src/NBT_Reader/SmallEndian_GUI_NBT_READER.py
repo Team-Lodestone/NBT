535f3b6aaf9250e7a1974ef8c46eb7288dbb46c9
Wendellmeset <69725697+wendellmeset@users.noreply.github.com>
2023-01-27 20:36:59 -0800

    Create SmallEndian_GUI_NBT_READER.py

---
+++ src/NBT_Reader/SmallEndian_GUI_NBT_READER.py
@@ -0,0 +1,27 @@
+import tkinter as tk
+from tkinter import ttk
+import nbtlib
+
+def print_tags_gui(parent, tag, treeview):
+    """
+    A function to recursively add all the tags in an NBT file to a treeview.
+    """
+    if isinstance(tag, nbtlib.tag.Compound):
+        for key, value in tag.items():
+            child = treeview.insert(parent, 'end', text=key)
+            print_tags_gui(child, value, treeview)
+    elif isinstance(tag, nbtlib.tag.List):
+        for value in tag:
+            child = treeview.insert(parent, 'end', text=str(value))
+            print_tags_gui(child, value, treeview)
+    else:
+        treeview.insert(parent, 'end', text=str(tag))
+
+root = tk.Tk()
+treeview = ttk.Treeview(root)
+treeview.pack(expand=True, fill='both')
+
+nbt_file = nbtlib.load('SimpleNBT.nbt', byteorder='little')
+print_tags_gui('', nbt_file, treeview)
+
+root.mainloop()
